4f9075051991604f7211deffa66c328cd078f81b
Jenny Robertson <jenny.robertson@bbc.co.uk>
2017-07-07 20:25:30 +0100

    Added all loves

--- src/responses/loves.js
+++ src/responses/loves.js
@@ -1,10 +1,41 @@
 /* eslint-disable  func-names */
 /* eslint-disable max-len */
-/* eslint quote-props: ['error', 'consistent']*/
+/* eslint quote-props: ['error, and all Universal Loves.', 'consistent']*/
 module.exports = {
     'LOVES_EN_GB': {
-        'alex': 'Alex loves Complete Breakfast, Salmon Dinner, and all Universal Loves',
-        'elliott': 'Elliot loves Crab Cakes, Duck Feathers, Lobsters, Pomegranates, Tom Kha Soup, and all Universal Loves',
-        'harvey': 'Harvey loves Coffee, Pickles, Super Meal, Truffle Oil, Wine, and all Universal Loves'
+        'Alex': 'Alex loves Complete Breakfast, Salmon Dinner, and all Universal Loves.',
+        'Elliott': 'Elliot loves Crab Cakes, Duck Feathers, Lobsters, Pomegranates, Tom Kha Soup.',
+        'Harvey': 'Harvey loves Coffee, Pickles, Super Meal, Truffle Oil, Wine, and all Universal Loves.',
+        'Sam': 'Sam loves Cactus Fruit, Maple Bar, Pizza and Tigerseye, and all Universal Loves.',
+        'Sebastian': 'Sebastian loves Frozen Tear, Obsidian, Pumpkin Soup, Sashimi and Void Egg, and all Universal Loves.',
+        'Shane': 'Shane loves Hot Peppers, Pepper Poppers, Pizzas, and all Universal Loves.',
+        'Abigail': 'Abigail loves Amethyst, Blackberry Cobbler, Chocolate Cake, Pufferfish, Pumpkin, Spicy Eel, and all Universal Loves.',
+        'Emily': 'Emily loves Amethyst, Aquamarine, Cloth, Emerald, Jade, Ruby, Survival Burger, Topaz, Wool , and all Universal Loves.',
+        'Haley': 'Hale Coconut Fruit Salad Pink Cak Sunflower, and all Universal Loves.',
+        'Leah': 'Leah loves Goat Cheese, Poppyseed, Muffi Salad ,Stir Fry, Truffle, Vegetable Medle Wine, and all Universal Loves.',
+        'Maru': 'Maru love Battery Packs, Cauliflower Cheese, Cauliflower, Diamond, Gold Bar, Iridium Bar, Mine Miner\'s Treat, Pepper Poppers, Rhubarb Pie, Strawberry, and all Universal Loves.',
+        'Penny': 'Penny loves Diamond, Emerald, Melon, Poppy, Poppyseed Muffin, Red Plate, Roots Platter, Sandfish, Tom Kha Soup, and all Universal Loves.',
+        'Caroline': 'Caroline love Fish Taco Summer Spangle, and all Universal Loves.',
+        'Clint': 'Clint loves Amethys, Aquamarine, Artichoke Dip, Emerald, Fiddlehead Risotto, Gold Bar, Iridium Bar, Jade, Omni Geode, Ruby, Topaz, and all Universal Loves.',
+        'Demetrius': 'Demetrius loves Bean Hotpot, Ice Cream, Rice Pudding, Strawberry, and all Universal Loves.',
+        'Evelyn': 'Evelyn loves Beet, Chocolate Cake, Diamonds, Fairy Roses, Stuffing, Tulips, and all Universal Loves.',
+        'George': 'George loves Fried Mushrooms, Leeks, and all Universal Loves.',
+        'Gus': 'Gus loves Diamonds, Escargot, Fish Tacos, Oranges, and all Universal Loves.',
+        'Jas': 'Jas loves Fairy Rose, Pink Cake, Plum Pudding, and all Universal Loves.',
+        'Jodi': 'Jodi loves Chocolate Cake, Crispy Bass, Diamond, Eggplant Parmesan, Fried Eel, Pancakes, Rhubarb Pie, Vegetable Medley, and all Universal Loves.',
+        'Kent': 'Kent loves Fiddlehead Risotto, Roasted Hazelnuts, and all Universal Loves.',
+        'Lewis': 'Lewis loves Autum Autumn\'s Bounty, Glazed Yams, Hot Peppers, Vegetable Medley, and all Universal Loves.',
+        'Linus': 'Linus loves Blueberry Tart, Cactus Fruit, Coconut, Dish o Dish o\' The Sea, Yams, and all Universal Loves.',
+        'Marnie': 'Marnie loves Diamonds, Farmer\'s Lunch, Pink Cake, Pumpkin Pie, and all Universal Loves.',
+        'Pam': 'Pam loves Beer, Cactus Fruit, Glazed Yam, Mead, Pale Ale, Parsnip, Parsnip Soup, and all Universal Loves.',
+        'Pierre': 'Pierre loves Fried Calamari, and all Universal Loves.',
+        'Robin': 'Robin loves Goat Cheese, Peaches, Spaghetti, and all Universal Loves.',
+        'Vincent': 'Vincent loves Cranberry Candy, Grapes, Pink Cake, and all Universal Loves.',
+        'Willy': 'Willy loves Catfish, Diamonds, Iridium Bars, Mead, Octopus, Pumpkins, Sea Cucumbers, Sturgeon, and all Universal Loves.',
+        'Dwarf': 'The Dwarf loves Amethys, Aquamarine, Emerald, Jade, Omni Geode, Ruby, Topaz, and all Universal Loves.',
+        'Krobus': 'Krobu s loves Diamonds, Iridium Bars, Pumpkins, Void Eggs, Void Mayonnaise, Wild Horseradish, and all Universal Loves.',
+        'Sandy': 'Sandy loves Crocuses, Daffodils, Sweet Peas, and all Universal Loves.',
+        'Wizard': 'Wizard loves Purple Mushrooms, Solar Essence, Super Cucumbers, Void Essence, and all Universal Loves.',
+
     }
 };
