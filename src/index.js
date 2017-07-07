7e4a53b1ed0800cd351f4a547e1e62e6c3cb1482
Jenny Robertson <jenny.robertson@bbc.co.uk>
2017-07-07 19:51:31 +0100

    Added loves

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@
 const Alexa = require('alexa-sdk');
 const homes = require('./responses/homes');
 const birthdays = require('./responses/birthdays');
+const loves = require('./responses/loves');
+
 
 const APP_ID = 'amzn1.ask.skill.1a184836-9acf-46bd-9e7b-0b6b9cc730c3';
 
@@ -40,6 +42,13 @@ const languageStrings = {
             BIRTHDAYS_NOT_FOUND_WITH_ITEM_NAME: ' %s\ birthday',
             BIRTHDAYS_NOT_FOUND_WITHOUT_ITEM_NAME: 'that villager',
             BIRTHDAYS_NOT_FOUND_REPROMPT: '. What else can I help with?',
+
+            LOVES: loves.LOVES_EN_GB,
+            LOVES_REPEAT_MESSAGE: 'Try saying repeat.',
+            LOVES_NOT_FOUND_MESSAGE: "I\'m sorry, I currently don\'t know ",
+            LOVES_NOT_FOUND_WITH_ITEM_NAME: ' %s\ birthday',
+            LOVES_NOT_FOUND_WITHOUT_ITEM_NAME: 'that villager',
+            LOVES_NOT_FOUND_REPROMPT: '. What else can I help with?',
         },
     },
     'en-GB': {
@@ -58,69 +67,11 @@ const handlers = {
         this.attributes.repromptSpeech = this.t('WELCOME_REPROMT');
         this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
     },
-    'HomeIntent': function () {
-        const villagerSlot = this.event.request.intent.slots.Villager;
-        let villagerName;
-        if (villagerSlot && villagerSlot.value) {
-            villagerName = villagerSlot.value.toLowerCase();
-        }
+    'HomeIntent': genericIntent('homes'),
 
-        const cardTitle = this.t('DISPLAY_CARD_TITLE', this.t('SKILL_NAME'), villagerName);
-        const homes = this.t('HOMES');
-        const home = homes[villagerName];
-
-        if (home) {
-            this.attributes.speechOutput = home;
-            this.attributes.repromptSpeech = this.t('HOMES_REPEAT_MESSAGE');
-            this.emit(':askWithCard', home, this.attributes.repromptSpeech, cardTitle, home);
-        } else {
-            let speechOutput = this.t('HOMES_NOT_FOUND_MESSAGE');
-            const repromptSpeech = this.t('HOMES_NOT_FOUND_REPROMPT');
-            if (villagerName) {
-                speechOutput += this.t('HOMES_NOT_FOUND_WITH_ITEM_NAME', villagerName);
-            } else {
-                speechOutput += this.t('HOME_NOT_FOUND_WITHOUT_ITEM_NAME');
-            }
-            speechOutput += repromptSpeech;
-
-            this.attributes.speechOutput = speechOutput;
-            this.attributes.repromptSpeech = repromptSpeech;
-
-            this.emit(':ask', speechOutput, repromptSpeech);
-        }
-    },
     'BirthdayIntent': genericIntent('birthdays'),
-    //  'BirthdayIntent': function () {
-    //     const villagerSlot = this.event.request.intent.slots.Villager;
-    //     let villagerName;
-    //     if (villagerSlot && villagerSlot.value) {
-    //         villagerName = villagerSlot.value.toLowerCase();
-    //     }
 
-    //     const cardTitle = this.t('DISPLAY_CARD_TITLE', this.t('SKILL_NAME'), villagerName);
-    //     const birthdays = this.t('BIRTHDAYS');
-    //     const birthday = birthdays[villagerName];
-
-    //     if (birthday) {
-    //         this.attributes.speechOutput = birthday;
-    //         this.attributes.repromptSpeech = this.t('BIRTHDAYS_REPEAT_MESSAGE');
-    //         this.emit(':askWithCard', birthday, this.attributes.repromptSpeech, cardTitle, birthday);
-    //     } else {
-    //         let speechOutput = this.t('BIRTHDAYS_NOT_FOUND_MESSAGE');
-    //         const repromptSpeech = this.t('BIRTHDAYS_NOT_FOUND_REPROMPT');
-    //         if (villagerName) {
-    //             speechOutput += this.t('BIRTHDAYS_NOT_FOUND_WITH_ITEM_NAME', villagerName);
-    //         } else {
-    //             speechOutput += this.t('BIRTHDAYS_NOT_FOUND_WITHOUT_ITEM_NAME');
-    //         }
-    //         speechOutput += repromptSpeech;
-
-    //         this.attributes.speechOutput = speechOutput;
-    //         this.attributes.repromptSpeech = repromptSpeech;
-
-    //         this.emit(':ask', speechOutput, repromptSpeech);
-    //     }
-    // },
+    'LoveIntent': genericIntent('loves'),
 
     'AMAZON.HelpIntent': function () {
         this.attributes.speechOutput = this.t('HELP_MESSAGE');
---
+++ src/responses/loves.js
@@ -0,0 +1,10 @@
+/* eslint-disable  func-names */
+/* eslint-disable max-len */
+/* eslint quote-props: ['error', 'consistent']*/
+module.exports = {
+    'LOVES_EN_GB': {
+        'alex': 'Alex loves Complete Breakfast, Salmon Dinner, and all Universal Loves',
+        'elliott': 'Elliot loves Crab Cakes, Duck Feathers, Lobsters, Pomegranates, Tom Kha Soup, and all Universal Loves',
+        'harvey': 'Harvey loves Coffee, Pickles, Super Meal, Truffle Oil, Wine, and all Universal Loves'
+    }
+};
